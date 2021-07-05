e8ff0deadc793718aea4c98844c44ac0a2d52e89
jo-member <68108633+jo-member@users.noreply.github.com>
2021-07-05 09:54:55 +0900

    change resize

--- code/inference_v2.py
+++ code/inference_v2.py
@@ -25,7 +25,7 @@ class TestDataset(Dataset):
         self.tokenizer = transformers.XLMRobertaTokenizer.from_pretrained('xlm-roberta-large')
         self.max_token = 30
         self.transform = transforms.Compose([
-            transforms.Resize((224, 224)),
+            transforms.Resize((356, 356)),
             transforms.ToTensor(),
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
         ])
